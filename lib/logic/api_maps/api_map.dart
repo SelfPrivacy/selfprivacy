a77c11f06c5920494321b282fa1bc7411a74cf99
Kherel <kherel@gmail.com>
2021-01-18 10:40:14 +0100

    fix

--- lib/logic/api_maps/api_map.dart
+++ lib/logic/api_maps/api_map.dart
@@ -36,7 +36,7 @@ class ConsoleInterceptor extends InterceptorsWrapper {
     addMessage(
       Message(
         text:
-            'response-uri: ${response.request.uri}\ncode: ${response.statusCode}\ndata: ${response.data}\n',
+            'response-uri: ${response.request.uri}\ncode: ${response.statusCode}\ndata: ${response.data.toString}\n',
       ),
     );
     return super.onResponse(response);

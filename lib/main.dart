8de33ea19b289f55b487767a12826a5d7003ed16
inexcode <inex.code@selfprivacy.org>
2022-02-10 12:50:37 +0300

    Fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:selfprivacy/config/hive_config.dart';
 import 'package:selfprivacy/ui/pages/initializing/initializing.dart';
 import 'package:selfprivacy/ui/pages/onboarding/onboarding.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:selfprivacy/ui/pages/rootRoute.dart';
 import 'package:wakelock/wakelock.dart';
 
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
           var appSettings = context.watch<AppSettingsCubit>().state;
 
           return AnnotatedRegion<SystemUiOverlayStyle>(
-            value: SystemUiOverlayStyle.light, // Manually changnig appbar color
+            value: SystemUiOverlayStyle.light, // Manually changing appbar color
             child: MaterialApp(
               scaffoldMessengerKey:
                   getIt.get<NavigationService>().scaffoldMessengerKey,

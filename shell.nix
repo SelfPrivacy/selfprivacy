856e26bd9e614fa1a93e8f8d5106e2ad8c65dc3a
Alya Sirko <alya@selfprivacy.org>
2022-08-27 17:37:48 +0300

    use unstable flutter thru channel

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 { pkgs ? import <nixpkgs> { config.android_sdk.accept_license = true; } }:
 
 let
+  unstable = import (fetchTarball "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz") {};
+
   androidComposition = pkgs.androidenv.composeAndroidPackages {
     toolsVersion = "26.1.1";
     platformToolsVersion = "33.0.2";
@@ -10,5 +12,5 @@ in
 pkgs.mkShell {
   ANDROID_HOME= "${androidComposition.androidsdk}/libexec/android-sdk";
   ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-  nativeBuildInputs = with pkgs; [ flutter openjdk11_headless ];
+  nativeBuildInputs = with pkgs; [ unstable.flutter openjdk11_headless ];
 }

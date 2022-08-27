53ef810a0923900ccb8c2c86897f0f64f3d329ba
Alya Sirko <alya@selfprivacy.org>
2022-08-27 18:22:58 +0300

    final version of ci spec

--- shell.nix
+++ shell.nix
@@ -1,16 +1,15 @@
-{ pkgs ? import <nixpkgs> { config.android_sdk.accept_license = true; } }:
+{ pkgs ? import <unstable> { config.android_sdk.accept_license = true; } }:
 
 let
-  unstable = import (fetchTarball "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz") {};
-
   androidComposition = pkgs.androidenv.composeAndroidPackages {
     toolsVersion = "26.1.1";
     platformToolsVersion = "33.0.2";
-    buildToolsVersions = [ "33.0.0" ];
+    buildToolsVersions = [ "30.0.3" ];
+    platformVersions = [ "31" "30" "29" ];
   };
 in
 pkgs.mkShell {
   ANDROID_HOME= "${androidComposition.androidsdk}/libexec/android-sdk";
   ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-  nativeBuildInputs = with pkgs; [ unstable.flutter openjdk11_headless ];
+  nativeBuildInputs = with pkgs; [ flutter openjdk11_headless ];
 }

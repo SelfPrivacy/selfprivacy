eb9d8a704aaa86b9b0c33a8283ee50a4b3631b92
Alya Sirko <alya@selfprivacy.org>
2022-08-27 16:54:11 +0300

    add android sdk

--- shell.nix
+++ shell.nix
@@ -1,5 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { config.android_sdk.accept_license = true; } }:
 
+let
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    toolsVersion = "26.1.1";
+    platformToolsVersion = "33.0.2";
+    buildToolsVersions = [ "33.0.0" ];
+  };
+in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ flutter dart ];
+  ANDROID_HOME= "${androidComposition.androidsdk}/libexec/android-sdk";
+  ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+  nativeBuildInputs = with pkgs; [ flutter dart openjdk11 ];
 }

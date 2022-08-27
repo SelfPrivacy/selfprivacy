d278f0f06bcc7d4710d8487d747bb79a702d905d
Alya Sirko <alya@selfprivacy.org>
2022-08-27 17:11:47 +0300

    delete unneded packages

--- shell.nix
+++ shell.nix
@@ -10,5 +10,5 @@ in
 pkgs.mkShell {
   ANDROID_HOME= "${androidComposition.androidsdk}/libexec/android-sdk";
   ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-  nativeBuildInputs = with pkgs; [ flutter dart openjdk11 ];
+  nativeBuildInputs = with pkgs; [ flutter ];
 }

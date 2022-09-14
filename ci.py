a3e860e2478cc0c0058d3663346296ce46ecc89c
Alya Sirko <alya@selfprivacy.org>
2022-09-14 16:21:46 +0300

    Better CI

--- ci.py
+++ ci.py
@@ -1,43 +1,39 @@
-#!/usr/bin/env python3
+\#!/usr/bin/env python3
 
 import os
 import subprocess
 import yaml
 import argparse
 
-CONTAINER_IMAGE = "localhost/flutter-build-env"
+CONTAINER_IMAGE = "docker.io/alyasirko/flutter-build-env"
 HOST_HOME = "/var/lib/drone-runner-exec"
-CONTAINER_HOME = "/var/lib/builder"
+CONTAINER_HOME = "/tmp/builder"
 
 APP_NAME = "pro.kherel.selfprivacy"
 APP_VERSION_FULL = yaml.safe_load(open("pubspec.yaml", "r"))['version']
 APP_SEMVER = APP_VERSION_FULL[:APP_VERSION_FULL.find("+")]
 APP_BUILD_ID = APP_VERSION_FULL[APP_VERSION_FULL.find("+"):][1::]
 
-HOST_MOUNTED_VOLUME = f"{HOST_HOME}/.local/share/containers/storage/volumes/src/_data"
+HOST_MOUNTED_VOLUME = f"{HOST_HOME}/.local/share/containers/storage/volumes/release/_data"
 
 # Environments
 
 def podman_offline(dir, *args):
   subprocess.run(["podman", "run", "--rm", "--network=none", f"--workdir={dir}",
-                  "-v", f"src:{CONTAINER_HOME}/src:U",
-                  "-v", f"{HOST_HOME}/fdroid:{CONTAINER_HOME}/fdroid:U",
-                  "-v", f"{HOST_HOME}/fdroid-keystore:{CONTAINER_HOME}/fdroid/fdroid-keystore:U",
-                  "-v", f"{HOST_HOME}/standalone-keystore:{CONTAINER_HOME}/fdroid/standalone-keystore:U",
+                  "-v", os.getcwd() + f":{CONTAINER_HOME}/src",
+                  "-v", f"{HOST_HOME}/fdroid:{CONTAINER_HOME}/fdroid",
+                  "-v", f"{HOST_HOME}/fdroid-keystore:{CONTAINER_HOME}/fdroid/fdroid-keystore",
+                  "-v", f"{HOST_HOME}/standalone-keystore:{CONTAINER_HOME}/fdroid/standalone-keystore",
                   "--env", "FDROID_KEYSTORE_PASS=" + os.environ.get('FDROID_KEYSTORE_PASS'),
                   "--env", "STANDALONE_KEYSTORE_PASS=" + os.environ.get('STANDALONE_KEYSTORE_PASS'),
+                  "--user", os.getuid() + ":" + os.getgid(), "--userns=keep-id",
                   CONTAINER_IMAGE, "bash", "-c", ' '.join(args)
                  ])
 
 def podman_online(dir, *args):
   subprocess.run(["podman", "run", "--rm", "--privileged", f"--workdir={dir}",
-                  "-v", f"src:{CONTAINER_HOME}/src:U",
-                  CONTAINER_IMAGE, "bash", "-c", ' '.join(args)
-                 ])
-
-def podman_ci(dir, *args):
-  subprocess.run(["podman", "run", "--rm", "--privileged", f"--workdir={dir}",
-                  "-v", os.getcwd() + f":{CONTAINER_HOME}/src:U",
+                  "-v", os.getcwd() + f":{CONTAINER_HOME}/src",
+                  "--user", os.getuid() + ":" + os.getgid(), "--userns=keep-id",
                   CONTAINER_IMAGE, "bash", "-c", ' '.join(args)
                  ])
 
@@ -52,11 +48,11 @@ def build_apk():
   podman_offline(f"{CONTAINER_HOME}/src", "flutter build apk")
 
 def sign_apk_standalone():
-  podman_offline(f"{CONTAINER_HOME}/fdroid",
-                 "zipalign -f -v 4 ../src/build/app/outputs/flutter-apk/app-release.apk",
+  podman_offline(f"{CONTAINER_HOME}/src",
+                 "zipalign -f -v 4 build/app/outputs/flutter-apk/app-release.apk",
                  f"standalone_{APP_NAME}-{APP_SEMVER}.apk")
   podman_offline(f"{CONTAINER_HOME}/fdroid",
-                 "apksigner sign --ks standalone-keystore --ks-key-alias standalone --ks-pass",
+                 "apksigner sign --ks ../fdroid/standalone-keystore --ks-key-alias standalone --ks-pass",
                  f"env:STANDALONE_KEYSTORE_PASS standalone_{APP_NAME}-{APP_SEMVER}.apk")
 
 def sign_apk_fdroid():
@@ -83,8 +79,8 @@ def deploy_gitea_release():
                   "--url", "https://git.selfprivacy.org"])
   subprocess.run(["tea", "releases", "create", "--repo", os.environ.get('DRONE_REPO'),
                   "--tag", os.environ.get('DRONE_SEMVER'), "--title", os.environ.get('DRONE_SEMVER'),
-                  "--asset", f"{HOST_HOME}/fdroid/standalone_{APP_NAME}-{APP_SEMVER}.apk",
-                  "--asset", f"{HOST_HOME}/fdroid/standalone_{APP_NAME}-{APP_SEMVER}.apk.idsig",
+                  "--asset", f"{HOST_MOUNTED_VOLUME}/standalone_{APP_NAME}-{APP_SEMVER}.apk",
+                  "--asset", f"{HOST_MOUNTED_VOLUME}/standalone_{APP_NAME}-{APP_SEMVER}.apk.idsig",
                   "--asset", f"{HOST_MOUNTED_VOLUME}/SelfPrivacy-{APP_SEMVER}-x86_64.AppImage",
                   "--asset", f"{HOST_MOUNTED_VOLUME}/SelfPrivacy-{APP_SEMVER}-x86_64.AppImage.zsync",
                   "--asset", f"{HOST_MOUNTED_VOLUME}/{APP_NAME}-{APP_SEMVER}.flatpak",
@@ -96,9 +92,14 @@ def deploy_fdroid_repo():
                       scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -r {HOST_HOME}/fdroid/repo/* deployer@selfprivacy.org:/var/www/fdroid.selfprivacy.org
                   """], shell=True)
 
-def run_ci_build():
-  podman_ci(f"{CONTAINER_HOME}/src", "flutter build linux --debug")
-  podman_ci(f"{CONTAINER_HOME}/src", "flutter build apk --debug")
+def ci_build_linux():
+  podman_online(f"{CONTAINER_HOME}/src", "flutter build linux --debug")
+
+def ci_build_apk():
+  podman_online(f"{CONTAINER_HOME}/src", "flutter build apk --debug")
+
+def ci_run_tests():
+  podman_online(f"{CONTAINER_HOME}/src", "flutter test")
 
 # Arguments
 
@@ -114,7 +115,9 @@ if __name__ == "__main__":
   group.add_argument("--package-linux-archive", action="store_true")
   group.add_argument("--deploy-gitea-release", action="store_true", help="depends on $GITEA_RELEASE_TOKEN")
   group.add_argument("--deploy-fdroid-repo", action="store_true", help="depends on $SSH_PRIVATE_KEY")
-  group.add_argument("--run-ci-build", action="store_true")
+  group.add_argument("--ci-build-linux", action="store_true")
+  group.add_argument("--ci-build-apk", action="store_true")
+  group.add_argument("--ci-run-tests", action="store_true")
   args = parser.parse_args()
 
 if args.build_linux:
@@ -135,5 +138,9 @@ elif args.deploy_gitea_release:
   deploy_gitea_release()
 elif args.deploy_fdroid_repo:
   deploy_fdroid_repo()
-elif args.run_ci_build:
-  run_ci_build()
+elif args.ci_build_linux:
+  ci_build_linux()
+elif args.ci_build_apk:
+  ci_build_apk()
+elif args.ci_run_tests:
+  ci_run_tests()

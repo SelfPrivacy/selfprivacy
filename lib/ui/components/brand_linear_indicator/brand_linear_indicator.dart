6f6df3076788bc4403d3b077f41f2e280c9d54d1
Inex Code <inex.code@selfprivacy.org>
2022-09-16 15:00:15 +0300

    Animate BrandLinearIndicator

--- lib/ui/components/brand_linear_indicator/brand_linear_indicator.dart
+++ lib/ui/components/brand_linear_indicator/brand_linear_indicator.dart
@@ -15,21 +15,29 @@ class BrandLinearIndicator extends StatelessWidget {
   final double height;
 
   @override
-  Widget build(final BuildContext context) => Container(
-        height: height,
-        width: double.infinity,
-        clipBehavior: Clip.antiAlias,
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(height),
-        ),
-        alignment: Alignment.centerLeft,
-        child: FractionallySizedBox(
-          widthFactor: value,
-          child: Container(
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(height),
+  Widget build(final BuildContext context) => LayoutBuilder(
+        builder: (final context, final constraints) => Container(
+          height: height,
+          width: constraints.maxWidth,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(height),
+          ),
+          alignment: Alignment.centerLeft,
+          child: AnimatedSlide(
+            duration: const Duration(milliseconds: 400),
+            curve: Curves.easeInOut,
+            offset: Offset(
+              -(1 - value),
+              0,
+            ),
+            child: Container(
+              width: constraints.maxWidth,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(height),
+              ),
             ),
           ),
         ),

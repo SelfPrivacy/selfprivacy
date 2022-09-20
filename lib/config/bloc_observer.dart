e15026ae25f93d472fe5fd526df52117dce3d361
Inex Code <inex.code@selfprivacy.org>
2022-09-20 12:44:05 +0300

    Disable block error showing snack bar

--- lib/config/bloc_observer.dart
+++ lib/config/bloc_observer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:selfprivacy/config/get_it_config.dart';
+// import 'package:selfprivacy/config/get_it_config.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   SimpleBlocObserver();
@@ -11,9 +11,9 @@ class SimpleBlocObserver extends BlocObserver {
     final Object error,
     final StackTrace stackTrace,
   ) {
-    getIt<NavigationService>().showSnackBar(
-      'Bloc error: ${error.toString()}',
-    );
+    // getIt<NavigationService>().showSnackBar(
+    //   'Bloc error: ${error.toString()}',
+    // );
     super.onError(bloc, error, stackTrace);
   }
 }

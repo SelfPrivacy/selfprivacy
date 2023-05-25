8863dc8b2c5f4914a831ca2fc5517b5835fda040
NaiJi <naijiworld@protonmail.com>
2023-05-24 23:51:40 -0300

    chore: Move DNS validation to provider layer for desec

--- lib/logic/providers/dns_providers/desec.dart
+++ lib/logic/providers/dns_providers/desec.dart
@@ -1,5 +1,6 @@
 import 'package:selfprivacy/logic/api_maps/generic_result.dart';
 import 'package:selfprivacy/logic/api_maps/rest_maps/dns_providers/desec/desec_api.dart';
+import 'package:selfprivacy/logic/api_maps/rest_maps/dns_providers/desired_dns_record.dart';
 import 'package:selfprivacy/logic/models/hive/server_domain.dart';
 import 'package:selfprivacy/logic/models/json/dns_records.dart';
 import 'package:selfprivacy/logic/providers/dns_providers/dns_provider.dart';
@@ -255,5 +256,161 @@ class DesecDnsProvider extends DnsProvider {
       success: true,
       data: domains,
     );
+  }
+
+  @override
+  Future<GenericResult<List<DesiredDnsRecord>>> validateDnsRecords(
+    final ServerDomain domain,
+    final String ip4,
+    final String dkimPublicKey,
+  ) async {
+    final result = await getDnsRecords(domain: domain);
+    if (result.data.isEmpty || !result.success) {
+      return GenericResult(
+        success: result.success,
+        data: [],
+        code: result.code,
+        message: result.message,
+      );
+    }
+
+    final records = result.data;
+    final List<DesiredDnsRecord> foundRecords = [];
+    try {
+      final List<DesiredDnsRecord> desiredRecords =
+          getDesiredDnsRecords(domain.domainName, ip4, dkimPublicKey);
+      for (final DesiredDnsRecord record in desiredRecords) {
+        if (record.description == 'record.dkim') {
+          final DnsRecord foundRecord = records.firstWhere(
+            (final r) =>
+                ('${r.name}.${domain.domainName}' == record.name) &&
+                r.type == record.type,
+            orElse: () => DnsRecord(
+              name: record.name,
+              type: record.type,
+              content: '',
+              ttl: 800,
+              proxied: false,
+            ),
+          );
+          // remove all spaces and tabulators from
+          // the foundRecord.content and the record.content
+          // to compare them
+          final String? foundContent =
+              foundRecord.content?.replaceAll(RegExp(r'\s+'), '');
+          final String content = record.content.replaceAll(RegExp(r'\s+'), '');
+          if (foundContent == content) {
+            foundRecords.add(record.copyWith(isSatisfied: true));
+          } else {
+            foundRecords.add(record.copyWith(isSatisfied: false));
+          }
+        } else {
+          if (records.any(
+            (final r) =>
+                ('${r.name}.${domain.domainName}' == record.name ||
+                    record.name == '') &&
+                r.type == record.type &&
+                r.content == record.content,
+          )) {
+            foundRecords.add(record.copyWith(isSatisfied: true));
+          } else {
+            foundRecords.add(record.copyWith(isSatisfied: false));
+          }
+        }
+      }
+    } catch (e) {
+      print(e);
+      return GenericResult(
+        data: [],
+        success: false,
+        message: e.toString(),
+      );
+    }
+    return GenericResult(
+      data: foundRecords,
+      success: true,
+    );
+  }
+
+  @override
+  List<DesiredDnsRecord> getDesiredDnsRecords(
+    final String? domainName,
+    final String? ip4,
+    final String? dkimPublicKey,
+  ) {
+    if (domainName == null || ip4 == null) {
+      return [];
+    }
+    return [
+      DesiredDnsRecord(
+        name: '',
+        content: ip4,
+        description: 'record.root',
+      ),
+      DesiredDnsRecord(
+        name: 'api.$domainName',
+        content: ip4,
+        description: 'record.api',
+      ),
+      DesiredDnsRecord(
+        name: 'cloud.$domainName',
+        content: ip4,
+        description: 'record.cloud',
+      ),
+      DesiredDnsRecord(
+        name: 'git.$domainName',
+        content: ip4,
+        description: 'record.git',
+      ),
+      DesiredDnsRecord(
+        name: 'meet.$domainName',
+        content: ip4,
+        description: 'record.meet',
+      ),
+      DesiredDnsRecord(
+        name: 'social.$domainName',
+        content: ip4,
+        description: 'record.social',
+      ),
+      DesiredDnsRecord(
+        name: 'password.$domainName',
+        content: ip4,
+        description: 'record.password',
+      ),
+      DesiredDnsRecord(
+        name: 'vpn.$domainName',
+        content: ip4,
+        description: 'record.vpn',
+      ),
+      DesiredDnsRecord(
+        name: '',
+        content: '10 $domainName.',
+        description: 'record.mx',
+        type: 'MX',
+        category: DnsRecordsCategory.email,
+      ),
+      DesiredDnsRecord(
+        name: '_dmarc.$domainName',
+        content: '"v=DMARC1; p=none"',
+        description: 'record.dmarc',
+        type: 'TXT',
+        category: DnsRecordsCategory.email,
+      ),
+      DesiredDnsRecord(
+        name: '',
+        content: '"v=spf1 a mx ip4:$ip4 -all"',
+        description: 'record.spf',
+        type: 'TXT',
+        category: DnsRecordsCategory.email,
+      ),
+      if (dkimPublicKey != null)
+        DesiredDnsRecord(
+          name: 'selector._domainkey.$domainName',
+          content: '"$dkimPublicKey"',
+          description: 'record.dkim',
+          type: 'TXT',
+          category: DnsRecordsCategory.email,
+        ),
+    ];
   }
 }

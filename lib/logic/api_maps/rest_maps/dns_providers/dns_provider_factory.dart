6337889ab76cf67b0aecd3e4fa5f71b756f0f106
NaiJi <naijiworld@protonmail.com>
2023-06-19 11:59:11 -0300

    chore: Remove unneeded files

--- lib/logic/api_maps/rest_maps/dns_providers/dns_provider_factory.dart
+++
@@ -1,8 +0,0 @@
-import 'package:selfprivacy/logic/api_maps/rest_maps/dns_providers/dns_provider.dart';
-import 'package:selfprivacy/logic/api_maps/rest_maps/dns_providers/dns_provider_api_settings.dart';
-
-abstract class DnsProviderApiFactory {
-  DnsProviderApi getDnsProvider({
-    final DnsProviderApiSettings settings,
-  });
-}
--- lib/logic/api_maps/rest_maps/provider_api_settings.dart
+++
@@ -1,8 +0,0 @@
-class ProviderApiSettings {
-  const ProviderApiSettings({
-    this.hasLogger = false,
-    this.isWithToken = true,
-  });
-  final bool hasLogger;
-  final bool isWithToken;
-}

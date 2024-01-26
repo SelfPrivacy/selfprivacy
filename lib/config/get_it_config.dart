fa21bdf034827512a4c77c8177fbf0beb2ed9e4f
Inex Code <inex.code@selfprivacy.org>
2024-01-26 14:43:44 +0400

    refactor: Remove unused timer singleton

--- lib/config/get_it_config.dart
+++ lib/config/get_it_config.dart
@@ -2,12 +2,10 @@ import 'package:get_it/get_it.dart';
 import 'package:selfprivacy/logic/get_it/api_config.dart';
 import 'package:selfprivacy/logic/get_it/console.dart';
 import 'package:selfprivacy/logic/get_it/navigation.dart';
-import 'package:selfprivacy/logic/get_it/timer.dart';
 
 export 'package:selfprivacy/logic/get_it/api_config.dart';
 export 'package:selfprivacy/logic/get_it/console.dart';
 export 'package:selfprivacy/logic/get_it/navigation.dart';
-export 'package:selfprivacy/logic/get_it/timer.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -15,7 +13,6 @@ Future<void> getItSetup() async {
   getIt.registerSingleton<NavigationService>(NavigationService());
 
   getIt.registerSingleton<ConsoleModel>(ConsoleModel());
-  getIt.registerSingleton<TimerModel>(TimerModel());
   getIt.registerSingleton<ApiConfigModel>(ApiConfigModel()..init());
 
   await getIt.allReady();

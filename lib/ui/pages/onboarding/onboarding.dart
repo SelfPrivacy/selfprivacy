46910061ed886464e17127c1743b3515f4a5ef82
Inex Code <inex.code@selfprivacy.org>
2024-02-08 14:30:50 +0200

    ci: Update Windows build

--- lib/ui/pages/onboarding/onboarding.dart
+++ lib/ui/pages/onboarding/onboarding.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:selfprivacy/logic/cubit/app_settings/app_settings_cubit.dart';
 import 'package:selfprivacy/ui/pages/onboarding/views/views.dart';

702363b7fad194abc37243980e2f48e28b19161e
Inex Code <inex.code@selfprivacy.org>
2026-09-01 22:11:58 +0300

    test: Hive v1-v2 migration test

---
+++ test/unit/config/hive_migrations/v1_to_v2/seed.dart
@@ -0,0 +1,72 @@
+import 'package:hive_ce/hive.dart';
+import 'package:selfprivacy/config/hive_config.dart';
+import 'package:selfprivacy/logic/models/hive/backblaze_bucket.dart';
+import 'package:selfprivacy/logic/models/hive/backups_credential.dart';
+import 'package:selfprivacy/logic/models/hive/server_details.dart';
+import 'package:selfprivacy/logic/models/hive/server_domain.dart';
+
+const int v1ServerId = 42;
+const String v1ServerToken = 'server-provider-token';
+const String v1DnsToken = 'dns-provider-token';
+const String v1DomainName = 'migration.example.org';
+const String v1ServerLocation = 'fsn1';
+const String v1ServerType = 'cx22';
+
+Future<void> seedV1Layout() async {
+  final Box<dynamic> serverInstallationBox = await Hive.openBox(
+    BNames.serverInstallationBox,
+  );
+  final Box<dynamic> appSettingsBox = await Hive.openBox(BNames.appSettingsBox);
+
+  final ServerProviderVolume volume = ServerProviderVolume(
+    id: 84,
+    name: 'migration-volume',
+    sizeByte: 10 * 1024 * 1024 * 1024,
+    serverId: v1ServerId,
+    linuxDevice: '/dev/disk/by-id/migration-volume',
+    uuid: 'volume-uuid',
+    location: v1ServerLocation,
+  );
+  final ServerHostingDetails serverDetails = ServerHostingDetails(
+    ip4: '203.0.113.42',
+    id: v1ServerId,
+    createTime: DateTime.utc(2024, 6, 26),
+    startTime: DateTime.utc(2024, 6, 26, 1),
+    volume: volume,
+    apiToken: 'server-api-token',
+    provider: ServerProviderType.hetzner,
+  );
+  final ServerDomain serverDomain = ServerDomain(
+    domainName: v1DomainName,
+    provider: DnsProviderType.cloudflare,
+  );
+  final BackupsCredential backupsCredential = BackupsCredential(
+    keyId: 'backblaze-key-id',
+    applicationKey: 'backblaze-application-key',
+    provider: BackupsProviderType.backblaze,
+  );
+  final BackblazeBucket backblazeBucket = BackblazeBucket(
+    bucketId: 'backblaze-bucket-id',
+    bucketName: 'migration-bucket',
+    applicationKeyId: 'bucket-application-key-id',
+    applicationKey: 'bucket-application-key',
+    encryptionKey: 'bucket-encryption-key',
+  );
+
+  await serverInstallationBox.putAll(<String, dynamic>{
+    BNames.serverDetails: serverDetails,
+    BNames.hetznerKey: v1ServerToken,
+    BNames.serverProvider: ServerProviderType.hetzner,
+    BNames.serverLocation: v1ServerLocation,
+    BNames.serverTypeIdentifier: v1ServerType,
+    BNames.serverDomain: serverDomain,
+    BNames.cloudFlareKey: v1DnsToken,
+    BNames.dnsProvider: DnsProviderType.cloudflare,
+    BNames.backblazeCredential: backupsCredential,
+    BNames.backblazeBucket: backblazeBucket,
+  });
+  await appSettingsBox.put(BNames.databaseVersion, 1);
+
+  await serverInstallationBox.close();
+  await appSettingsBox.close();
+}
---
+++ test/unit/config/hive_migrations/v1_to_v2/v1_to_v2_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_ce/hive.dart';
+import 'package:selfprivacy/config/hive_config.dart';
+import 'package:selfprivacy/logic/models/hive/backblaze_bucket.dart';
+import 'package:selfprivacy/logic/models/hive/backups_credential.dart';
+import 'package:selfprivacy/logic/models/hive/dns_provider_credential.dart';
+import 'package:selfprivacy/logic/models/hive/server.dart';
+import 'package:selfprivacy/logic/models/hive/server_details.dart';
+import 'package:selfprivacy/logic/models/hive/server_domain.dart';
+import 'package:selfprivacy/logic/models/hive/server_provider_credential.dart';
+
+import '../../../../fakes/hive/in_memory_hive.dart';
+import 'seed.dart';
+
+void main() {
+  setUpAll(setUpInMemoryHive);
+  tearDownAll(tearDownInMemoryHive);
+
+  test('v1 to v2 moves installation resources into resourcesBox', () async {
+    await seedV1Layout();
+    await Hive.openBox(BNames.serverInstallationBox);
+    await Hive.openBox(BNames.resourcesBox);
+
+    await HiveConfig.performMigrations();
+
+    expect(
+      Hive.box(BNames.appSettingsBox).get(BNames.databaseVersion),
+      HiveConfig.version,
+    );
+
+    await Hive.box(BNames.resourcesBox).close();
+    final Box<dynamic> resourcesBox = await Hive.openBox(BNames.resourcesBox);
+
+    final List<ServerProviderCredential> serverCredentials =
+        List<ServerProviderCredential>.from(
+          resourcesBox.get(BNames.serverProviderTokens) as List<dynamic>,
+        );
+    expect(serverCredentials, hasLength(1));
+    expect(serverCredentials.single.tokenId, isNull);
+    expect(serverCredentials.single.token, v1ServerToken);
+    expect(serverCredentials.single.provider, ServerProviderType.hetzner);
+    expect(serverCredentials.single.associatedServerIds, <int>[v1ServerId]);
+
+    final List<Server> servers = List<Server>.from(
+      resourcesBox.get(BNames.servers) as List<dynamic>,
+    );
+    expect(servers, hasLength(1));
+    expect(servers.single.domain.domainName, v1DomainName);
+    expect(servers.single.domain.provider, DnsProviderType.cloudflare);
+    expect(servers.single.hostingDetails.id, v1ServerId);
+    expect(servers.single.hostingDetails.ip4, '203.0.113.42');
+    expect(servers.single.hostingDetails.apiToken, 'server-api-token');
+    expect(servers.single.hostingDetails.provider, ServerProviderType.hetzner);
+    expect(servers.single.hostingDetails.serverLocation, v1ServerLocation);
+    expect(servers.single.hostingDetails.serverType, v1ServerType);
+    expect(servers.single.hostingDetails.volume.id, 84);
+    expect(servers.single.hostingDetails.volume.uuid, 'volume-uuid');
+
+    final List<DnsProviderCredential> dnsCredentials =
+        List<DnsProviderCredential>.from(
+          resourcesBox.get(BNames.dnsProviderTokens) as List<dynamic>,
+        );
+    expect(dnsCredentials, hasLength(1));
+    expect(dnsCredentials.single.tokenId, isNull);
+    expect(dnsCredentials.single.token, v1DnsToken);
+    expect(dnsCredentials.single.provider, DnsProviderType.cloudflare);
+    expect(dnsCredentials.single.associatedDomainNames, <String>[v1DomainName]);
+
+    final List<BackupsCredential> backupsCredentials =
+        List<BackupsCredential>.from(
+          resourcesBox.get(BNames.backupsProviderTokens) as List<dynamic>,
+        );
+    expect(backupsCredentials, hasLength(1));
+    expect(backupsCredentials.single.keyId, 'backblaze-key-id');
+    expect(
+      backupsCredentials.single.applicationKey,
+      'backblaze-application-key',
+    );
+    expect(backupsCredentials.single.provider, BackupsProviderType.backblaze);
+
+    final BackblazeBucket bucket = resourcesBox.get(BNames.backblazeBucket);
+    expect(bucket.bucketId, 'backblaze-bucket-id');
+    expect(bucket.bucketName, 'migration-bucket');
+    expect(bucket.applicationKeyId, 'bucket-application-key-id');
+    expect(bucket.applicationKey, 'bucket-application-key');
+    expect(bucket.encryptionKey, 'bucket-encryption-key');
+
+    final Box<dynamic> serverInstallationBox = Hive.box(
+      BNames.serverInstallationBox,
+    );
+    expect(serverInstallationBox.get(BNames.hetznerKey), v1ServerToken);
+    expect(serverInstallationBox.get(BNames.cloudFlareKey), v1DnsToken);
+  });
+}
